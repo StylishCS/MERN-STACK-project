ebe4885bc9902ed25a69d8f459cbed49241736e7
StylishCS <118624912+StylishCS@users.noreply.github.com>
2023-07-07 20:37:33 +0300

    Delete routes directory

--- routes/signup.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const {/*getSignup,*/ postSignup} = require('../controllers/signupController');
-
-/* GET home page. */
-//router.get('/', getSignup);
-router.post('/', postSignup);
-
-module.exports = router;
